3c3b61b6d64865c4bb14cd3459943b3abe072c45
name <email>
2026-09-08 13:13:20 +0300

    struct T_dijkstra

--- graph/weighted.cpp
+++ graph/weighted.cpp
@@ -1,4 +1,5 @@
 
+#include <string>
 #include <vector>
 #include <queue>
 #include <iostream>
@@ -8,9 +9,29 @@ using namespace std;
 using p = pair<int, int>;
 
 
-struct Graph_W
-{
+struct T_dijkstra {
     static constexpr int inf = INT_MAX;
+    vector<int> dist;
+    int start;
+    void reset(const int v_num, const int start){
+        this->start = start;
+        dist.assign(v_num, inf);
+    }
+};
+
+ostream& operator<<(ostream& os, const T_dijkstra& t) {
+    os << "dijkstra\n\n  ";
+    for (int v = 0; v < t.dist.size(); ++v)
+        os << v << ' ';
+    os << '\n' << t.start << ' ';
+    for (int v = 0; v < t.dist.size(); ++v)
+        os << (t.dist[v] == t.inf ? "· " : to_string(t.dist[v]) + " ");
+    return os << "\n\n";
+}
+
+
+struct Graph_W {
+
     vector<vector<p>> adj; // {cost, vertex_to}
     
     Graph_W() = default;
@@ -21,72 +42,60 @@ struct Graph_W
     }
 
     void demo() {
-        *this = Graph_W(7);
-        add_e(0, 1, 1);
-        add_e(1, 2, 1); add_e(1, 3, 1);
-        add_e(3, 2, 1); add_e(3, 4, 1);
-        add_e(4, 2, 1);
-        add_e(5, 6, 1);
-        add_e(6, 4, 1);
+        *this = Graph_W(3);
+        add_e(0, 1, 1);   //  0 -> 1       (1)
+        add_e(1, 2, 1);   //       1 -> 2  (1)
+        add_e(0, 2, 7);   //  0 ->   -> 2  (7)
     }
 
-    vector<int> dijkstra(const int start)
-    {
-        vector<int> dist(adj.size(), inf);
+    void dijkstra(const int start, T_dijkstra& t) {
+
         priority_queue<p, vector<p>, greater<p>> pq; // {path, vertex}
+        t.reset(adj.size(), start);
     
-        pq.push({0, start});
-        dist[start] = 0;
+        pq.push({0, t.start});
+        t.dist[start] = 0;
     
         while (!pq.empty()) {
             p v = pq.top();
             pq.pop();
-            if (dist[v.second] != v.first)
+            if (t.dist[v.second] != v.first)
                 continue;
             for (p vv : adj[v.second]) {
-                int path = dist[v.second] + vv.first;
-                if (dist[vv.second] > path) {
-                    dist[vv.second] = path;
+                int path = t.dist[v.second] + vv.first;
+                if (t.dist[vv.second] > path) {
+                    t.dist[vv.second] = path;
                     pq.push({path, vv.second});
                 }
             }
         }
-    
-        return dist;
     }
 
     vector<int> prim();
     vector<int> kruskal();
 };
 
-
-ostream& operator<<(ostream& os, const vector<int>& a) {
-    for (int i = 0; i < a.size(); ++i)
-        os << a[i] << ' ';
-    return os << '\n';
-}
-
 ostream& operator<<(ostream& os, const Graph_W& gw) {
     os << "Graph_W\n\n";
     for (int i = 0; i < gw.adj.size(); ++i) {
         os << i << " -> ";
         for (int j = 0; j < gw.adj[i].size(); ++j)
-            os << gw.adj[i][j].second << '(' << gw.adj[i][j].first << ") ";
+            os << gw.adj[i][j].second << '{' << gw.adj[i][j].first << "} ";
         os << '\n';
     }
     return os << '\n';
 }
 
 
-int main()
-{
+int main() {
+
     Graph_W gw;
     gw.demo();
-
     cout << gw;
 
-    vector<int> dist = gw.dijkstra(0);
-    cout << dist << '\n';
+    T_dijkstra t;
+    gw.dijkstra(0, t);
+    cout << t;
 
     return 0;
 }
