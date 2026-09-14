a442d92d8a866fb4a56e347408a27d34e06d289a
name <email>
2026-09-14 16:54:47 +0300

    T_dijkstra.vec<vec<int>> dist

--- graph/weighted.cpp
+++ graph/weighted.cpp
@@ -7,8 +7,7 @@
 
 using namespace std;
 
-template<typename T>
-using vec = vector<T>;
+template<typename T> using vec = vector<T>;
 using Pair = pair<int, int>; // dijkstra
 
 
@@ -17,21 +16,32 @@ using Pair = pair<int, int>; // dijkstra
 
 struct T_dijkstra {
     static constexpr int inf = INT_MAX;
-    vec<int> dist;
-    int start;
-    void reset(const int v_num, const int start){
-        this->start = start;
-        dist.assign(v_num, inf);
+    vec<vec<int>> dist;
+    vec<bool>     done;
+    void reset(const int v_num) {
+        dist.assign(v_num, vec<int>(v_num, inf));
+        done.assign(v_num, false);
+    }
+    void reset(const int v_num, const int start) {
+        if (dist.size() != v_num)
+            reset(v_num);
+        done[start] = true;
     }
 };
 
 ostream& operator<<(ostream& os, const T_dijkstra& t) {
-    os << "dijkstra\n\n  ";
+    os << "dijkstra\n   ";
+
     for (int v = 0; v < t.dist.size(); ++v)
         os << v << ' ';
-    os << '\n' << t.start << ' ';
-    for (int v = 0; v < t.dist.size(); ++v)
-        os << (t.dist[v] == t.inf ? "· " : to_string(t.dist[v]) + " ");
+
+    for (int i = 0; i < t.dist.size(); ++i)
+        if (t.done[i]) {
+            os << "\n " << i << " ";
+            for (int d : t.dist[i])
+                os << (d == t.inf ? "· " : to_string(d) + " ");
+        }
+
     return os << "\n\n";
 }
 
@@ -47,11 +57,14 @@ struct Edge {
 
 struct Graph_W {
 
+    string name = "__empty";
+
     vec<vec<Edge>> adj_list; // adj_list[from] -> { (from, to, cost) }
     vec<vec<bool>> has_edge; // has_edge[from][to]
 
     Graph_W() = default;
-    Graph_W(const int v_num) {
+    Graph_W(const int v_num, const string name = "__no_name") {
+        this->name = name;
         adj_list = vec<vec<Edge>>(v_num);
         has_edge = vec<vec<bool>>(v_num, vec<bool>(v_num, false));
     }
@@ -69,9 +82,10 @@ struct Graph_W {
 
     void demo() {
         *this = Graph_W(3);
-        add_e(0, 1, 1);   //  0 -> 1       {1}
-        add_e(1, 2, 1);   //       1 -> 2  {1}
-        add_e(0, 2, 3);   //  0 ->   -> 2  {3}
+        name = "0 -> 1 -> 2 <- 0";
+        add_e(0, 1, 1);
+        add_e(1, 2, 1);
+        add_e(0, 2, 3);
     }
 
     // Add undirected edge
@@ -83,15 +97,18 @@ struct Graph_W {
         }
         has_edge[from][to] = true;
         adj_list[from].push_back(Edge(from, to, cost));
-        has_edge[to][from] = true;
-        adj_list[to].push_back(Edge(to, from, cost));
+        if (from != to) {
+            has_edge[to][from] = true;
+            adj_list[to].push_back(Edge(to, from, cost));
+        }
     }
 
     void demo_undirected() {
         *this = Graph_W(3);
-        add_ue(0, 1, 1);  // 0 - 1      {1}
-        add_ue(1, 2, 2);  //     1 - 2  {2}
-        add_ue(2, 0, 3);  // 0 -   - 2  {3}
+        name = "0 - 1 - 2 - 0";
+        add_ue(0, 1, 1);
+        add_ue(1, 2, 2);
+        add_ue(2, 0, 3);
     }
 
 
@@ -123,29 +140,36 @@ struct Graph_W {
         return true;
     }
 
-    void dijkstra(const int start, T_dijkstra& T) {
+    void dijkstra(T_dijkstra& t, const int start) {
     
         // {path, vertex}
         priority_queue<Pair, vec<Pair>, greater<Pair>> pq;
 
-        T.reset(adj_list.size(), start);
+        t.reset(adj_list.size(), start);
+
+        vec<int>& d = t.dist[start];
     
         pq.push({0, start});
-        T.dist[start] = 0;
+        d[start] = 0; // no match for ‘operator=’
     
         while (!pq.empty()) {
             Pair P = pq.top();
             pq.pop();
-            if (P.first > T.dist[P.second])
+            if (P.first > d[P.second])
                 continue;
             for (const Edge& E : adj_list[P.second]) {
-                int path = T.dist[P.second] + E.cost;
-                if (path < T.dist[E.to]) {
-                    T.dist[E.to] = path;
+                int path = d[P.second] + E.cost;
+                if (path < d[E.to]) {
+                    d[E.to] = path;
                     pq.push({path, E.to});
                 }
             }
         }
+    }
+
+    void dijkstra(T_dijkstra& t) {
+        for (int i = 0; i < adj_list.size(); ++i)
+            dijkstra(t, i);
     }
 
     vec<int> prim();
@@ -154,9 +178,10 @@ struct Graph_W {
 
 ostream& operator<<(ostream& os, const Graph_W& g) {
     os << (g.is_undirected() ? "Undirected" : "Directed")
-        << " Graph_W\n\n";
+        << " Graph_W \n"
+        << " name: \"" << g.name << "\" \n\n";
     for (int i = 0; i < g.adj_list.size(); ++i) {
-        os << i << " -> ";
+        os << " " << i << " -> ";
         for (int j = 0; j < g.adj_list[i].size(); ++j) {
             Edge E = g.adj_list[i][j];
             os << E.to << '{' << E.cost << "} ";
@@ -174,14 +199,18 @@ int main() {
     g.demo();
     cout << g;
 
-    T_dijkstra t_d;
-    g.dijkstra(0, t_d);
-    cout << t_d;
+    T_dijkstra t;
+    g.dijkstra(t);
+    cout << t;
 
     // Undirected graph for Prim() and Kruskal()
     Graph_W gg;
     gg.demo_undirected();
     cout << gg;
+
+    T_dijkstra tt;
+    gg.dijkstra(tt);
+    cout << tt;
 
     return 0;
 }
