0cec643ee714e1ff8fbcc4f20c15a3dcaac0b421
name <email>
2026-09-19 19:39:05 +0300

    git mv weighted.cpp

--- graph/build.sh
+++ graph/build.sh
@@ -6,6 +6,7 @@ mkdir build
 
 clear
 
-# g++ -g -O0 $(find . -name "*.cpp") -o build/bin;
+g++ -g -O0 graph_weighted/graph_weighted.cpp -o build/bin
 
-g++ -g -O0 weighted.cpp -o build/bin
+# g++ -g -O0 $(find . -name "*.cpp") -o build/bin;
+# g++ -g -O0 main.cpp graph/graph.cpp traversal/traversal.cpp
