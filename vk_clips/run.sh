c98f4812cd18f50170f8c5b0dc7c67007e625258
gpu-enjoyer <88340770+gpu-enjoyer@users.noreply.github.com>
2026-09-22 01:40:14 +0300

    Delete vk_clips directory

--- vk_clips/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-[ ! -d .venv ] || [ ! -d cache ] && ./install.sh
-
-.venv/bin/python src/get_html.py
-.venv/bin/python src/get_links.py
-.venv/bin/yt-dlp -a cache/links.txt -o "clips/%(id)s.%(ext)s" --merge-output-format mp4
